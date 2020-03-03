7fa61e0cd04183d5dbc903139bf5ef0ca724415d
haesleinhuepf <rhaase@mpi-cbg.de>
2020-03-03 10:27:37 +0100

    replaced html-based BISE seach by JSON based BIII search

---
+++ src/main/java/org/scijava/search/web/BIIIeuSearchResult.java
@@ -0,0 +1,9 @@
+package org.scijava.search.web;
+
+import java.util.Map;
+
+public class BIIIeuSearchResult extends WebSearchResult {
+    public BIIIeuSearchResult(String name, String url, String details, String icon, Map<String, String> extraProperties) {
+        super(name, url, details, icon, extraProperties);
+    }
+}
